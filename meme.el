662aaeaa5130608f889075cf2cf2e4aa248b8017
Lars Magne Ingebrigtsen <larsi@gnus.org>
2016-06-27 19:52:39 +0200

    Display the candidate images

--- meme.el
+++ meme.el
@@ -23,6 +23,48 @@
 
 ;;; Code:
 
+(require 'cl)
+
+(defun meme ()
+  "Create a meme image interactively in Emacs."
+  (interactive)
+  (pop-to-buffer (get-buffer-create "*meme*"))
+  (meme-mode)
+  (meme--insert-thumbnails))
+
+(defvar meme-mode-map
+  (let ((map (make-keymap)))
+    (set-keymap-parent map special-mode-map)
+    ;;(define-key map " " 'archive-next-line)
+    map))
+
+(define-derived-mode meme-mode meme-mode "Meme"
+  "Major mode for creating meme images.
+
+\\{meme-mode-map}")
+
+(defun meme--insert-thumbnails ()
+  (let* ((dir (expand-file-name
+	       "images" (file-name-directory (locate-library "meme"))))
+	 (pixels 180)
+	 (width (/ (- (nth 2 (window-pixel-edges))
+		      (nth 0 (window-pixel-edges))
+		      20)
+		   pixels))
+	 (i 0))
+    (erase-buffer)
+    (dolist (file (directory-files dir t ".jpg\\'"))
+      (unless (bolp)
+	(insert (propertize " " 'display
+			    `(space :align-to (,(* (mod i width) pixels))))))
+      (insert-image (create-image file 'imagemagick nil
+				  :max-width pixels
+				  :max-height pixels)
+		    " ")
+      (when (zerop (mod (incf i) width))
+	(insert "\n")))
+    (goto-char (point-min))))
+
 (provide 'meme)
 
 ;;; meme.el ends here
